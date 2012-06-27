7d73504c974ee864bd77972855d86e0975fb6c32
Meng ZHuo <mengzhuo1203@gmail.com>
2012-06-27 18:17:14 +0800

    Initial version

---
+++ extension.js
@@ -0,0 +1,99 @@
+const Main = imports.ui.main;
+const Meta = imports.gi.Meta;
+const Lang = imports.lang;
+const Clutter = imports.gi.Clutter;
+const Tweener = imports.ui.tweener;
+const ExtensionSystem = imports.ui.extensionSystem;
+const ExtensionUtils = imports.misc.extensionUtils;
+
+
+const CONLICT_UUID = [];
+const WINDOW_ANIMATION_TIME = 0.2;
+const ROTATION_ANGLE = 7;
+
+const RotateInForWindow = new Lang.Class({
+    
+    Name: "RotateInForWindow",
+    
+    _init: function (){
+        
+        this._display =  global.screen.get_display();
+        
+        this.signalConnectID = this._display.connect('window-created', Lang.bind(this, this._rotateIn));
+
+        global._rotate_in_aminator = this;
+        
+        this._x_half = global.screen_width/2;
+        this._y_half = global.screen_height/2;
+        
+    },
+    _rotateIn : function (display,window){
+        
+        if (!window.maximized_horizontally && window.get_window_type() == Meta.WindowType.NORMAL){
+            let actor = window.get_compositor_private();
+            
+            let [prevX,prevY] = actor.get_position();
+            let [width,height] = actor.get_size();
+            
+            let centerX = (prevX+Math.round(width/2));
+            let centerY = (prevY+Math.round(height/2));
+            
+            let x_flag = centerX - this._x_half;
+            let y_flag = centerY - this._y_half;
+            
+            let vertex = new Clutter.Vertex ({ x:(x_flag < 0 )?0:width,
+                                                y:(y_flag < 0 )?0:height,
+                                                z:0});
+            actor.rotation_center_z = vertex;
+            
+            //FIXME why rotation_angle_z won't work if we use it in Tweener directly?
+            actor._rz = ( x_flag*y_flag < 0 )?-ROTATION_ANGLE:ROTATION_ANGLE;
+
+            Tweener.addTween(actor,{
+                             _rz:0,
+                             onUpdateScope: actor,
+                             onUpdate: function(){
+                                actor.rotation_angle_z = actor._rz;
+                             },
+                             time: WINDOW_ANIMATION_TIME,
+                             transition: 'easeOutQuad'
+                            });
+            
+        };
+    },
+    destroy : function (){
+        delete global._rotate_in_aminator;
+        this._display.disconnect(this.signalConnectID);
+    },
+    _onDestroy : function (){
+        this.destroy();
+    }
+});
+
+rotatemaker = null;
+metadata = null;
+
+function enable() {
+    // check conflict extension
+    for (var item in ExtensionUtils.extensions){
+        
+        if (CONLICT_UUID.indexOf(item.uuid) >= 0 && item.state == ExtensionSystem.ExtensionState.ENABLED){
+            throw new Error('%s conflict with %s'.format(item,metadata.uuid));
+            rotatemaker = 'CONFLICTED';
+        }
+        
+    }
+    
+    if (rotatemaker == null){
+        rotatemaker = new RotateInForWindow();
+    }
+}
+function disable() {
+    if (rotatemaker != null){
+        rotatemaker.destroy();
+        rotatemaker = null;
+    }
+}
+function init(metadataSource) {
+    metadata = metadataSource;
+}
